96112ee37bab84f736a22045ad5b6f8954422de4
pzhen <624359088@qq.com>
2018-03-20 14:44:37 +0800

    helper

--- utils/utils_string.go
+++ utils/utils_string.go
@@ -1,1 +1,32 @@
 package utils
+
+import "strings"
+
+//截取字符串
+func Substr(s string, pos, length int) string {
+	runes := []rune(s)
+	l := pos + length
+	if l > len(runes) {
+		l = len(runes)
+	}
+	return string(runes[pos:l])
+}
+
+func Utf8Index(str, substr string) int {
+	asciiPos := strings.Index(str, substr)
+	if asciiPos == -1 || asciiPos == 0 {
+		return asciiPos
+	}
+	pos := 0
+	totalSize := 0
+	reader := strings.NewReader(str)
+	for _, size, err := reader.ReadRune(); err == nil; _, size, err = reader.ReadRune() {
+		totalSize += size
+		pos++
+		// 匹配到
+		if totalSize == asciiPos {
+			return pos
+		}
+	}
+	return pos
+}
